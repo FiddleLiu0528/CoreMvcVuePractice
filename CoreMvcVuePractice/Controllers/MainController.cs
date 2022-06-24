b5c2a91431ad0909b8d417f98215e632059d8d83
AJuA <zx90302xz@gmail.com>
2022-06-24 21:54:41 +0800

    調整 controllers 跟 views 的結構

---
+++ CoreMvcVuePractice/Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using CoreMvcVuePractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreMvcVuePractice.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
