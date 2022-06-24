c78e5b33cd2f1c2b06ae51236d9092334b71f346
I552\Administrator <fiddleliu0528@gmail.com>
2022-06-24 23:58:00 +0800

    調整 編譯 前端網頁流程 調整前端設定檔編排

--- CoreMvcVuePractice/ClientApp/login/vue.config.js
+++ CoreMvcVuePractice/ClientApp/login/vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/login/'
-    : '/',
-  indexPath: 'index'
+  publicPath: process.env.NODE_ENV === "production" ? "/login/" : "/",
+  indexPath: "index",
 });
