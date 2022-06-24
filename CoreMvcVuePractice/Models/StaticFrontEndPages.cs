ac052d23f9c5cc33411117b6011e8533b7ac3d46
I552\Administrator <fiddleliu0528@gmail.com>
2022-06-25 01:12:20 +0800

    調整 緩存靜態預設頁面

---
+++ CoreMvcVuePractice/Models/StaticFrontEndPages.cs
@@ -0,0 +1,13 @@
+﻿namespace CoreMvcVuePractice.Models
+{
+    public static class StaticFrontEndPages
+    {
+#if DEBUG
+        public readonly static string loginPage = File.ReadAllText("ClientApp/login/dist/index");
+        public readonly static string mainPage = File.ReadAllText("ClientApp/main/dist/index");
+#else
+        public readonly static string loginPage = File.ReadAllText("wwwroot/login/index");
+        public readonly static string mainPage = File.ReadAllText("wwwroot/main/index");
+#endif
+    }
+}
