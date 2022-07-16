af235a49444567657a6cbc7dfe74aadaf2df54be
Fiddle <fiddleliu0528@gmail.com>
2022-07-17 00:05:22 +0800

    添加 主頁面驗證失敗轉導

--- CoreMvcVuePractice/Controllers/MainController.cs
+++ CoreMvcVuePractice/Controllers/MainController.cs
@@ -16,10 +16,24 @@ namespace CoreMvcVuePractice.Controllers
 
         public IActionResult Index()
         {
-            var str = HttpContext.Session.GetString("UserSessionInfo") ?? string.Empty;
-            var obj = JsonConvert.DeserializeObject<UserSessionInfo>(str);
+            try
+            {
+                var userSessionInfoString = HttpContext.Session.GetString("UserSessionInfo") ?? string.Empty;
+                var userSessionInfo = JsonConvert.DeserializeObject<UserSessionInfo>(userSessionInfoString);
+                var isExist = userSessionInfo != null;
 
-            return View();
+                if (!isExist) return RedirectToAction("Index", "Login"); // 使用者資訊不存在(未登入)，則返回首頁
+
+                ViewBag.Account = userSessionInfo?.Account;
+                ViewBag.SessionId = userSessionInfo?.SessionId;
+
+                // 轉導頁面
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Login");
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
