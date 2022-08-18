9e7ab4699b64138d59abe54507d53273e78ab0f5
Fiddle <fiddleliu0528@gmail.com>
2022-08-18 09:12:39 +0800

    新增 會員列表功能 測試接口

---
+++ CoreMvcVuePractice/MockControllers/Api/MemberListController.cs
@@ -0,0 +1,197 @@
+﻿
+using CoreMvcVuePractice.Models;
+using Microsoft.AspNetCore.Mvc;
+using static CoreMvcVuePractice.Models.Dtos.MemberListControllerDtos;
+
+namespace CoreMvcVuePractice.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class MemberListController : ControllerBase
+    {
+        /// <summary>
+        /// 取得 等級權限設定列表
+        /// </summary>
+        [HttpPost]
+        public ResponseResult RetrieveMemberModeTypes()
+        {
+            try
+            {
+                return new ResponseResult<RetrieveMemberModeTypes_ResultDto>()
+                {
+                    ErrorCode = ErrorCode.Success,
+                    Result = new RetrieveMemberModeTypes_ResultDto()
+                    {
+                        MemberModeList = new List<MemberMode>() {
+                             new MemberMode()
+                            {
+                                 SerialNumber = 1,
+                                 Sort = 1,
+                                 Type = 0,
+                                 CreateDateTime = DateTime.Now,
+                                 GiftSettingIds = null,
+                                 IsMute = false,
+                                 Name = "name",
+                                 Remark = null
+                            }
+                        }
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Fail
+                };
+            }
+        }
+
+        /// <summary>
+        /// 取得 禁言原因列表
+        /// </summary>
+        [HttpPost]
+        public ResponseResult RetrieveBanReasonTypes()
+        {
+            try
+            {
+                return new ResponseResult<RetrieveBanReasonTypes_ResultDto>()
+                {
+                    ErrorCode = ErrorCode.Success,
+                    Result = new RetrieveBanReasonTypes_ResultDto()
+                    {
+                        BanReasonList = new List<BanReason>() {
+                             new BanReason()
+                             {
+                                CreateDateTime = DateTime.Now,
+                                Day = 1,
+                                Reason = "",
+                                SerialNumber = 1,
+                                Sort = 1,
+                             }
+                        }
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                //this.serverSideLogger.Error(ex, $"{this.GetType().Name} {MethodBase.GetCurrentMethod().Name} Exception");
+
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Fail
+                };
+            }
+        }
+
+        /// <summary>
+        /// 取得 所有會員及禁言 的數量
+        /// </summary>
+        [HttpPost]
+        public ResponseResult RetrieveMemberSum()
+        {
+            try
+            {
+                return new ResponseResult<RetrieveMemberSum_ResultDto>()
+                {
+                    ErrorCode = ErrorCode.Success,
+                    Result = new RetrieveMemberSum_ResultDto
+                    {
+                        MemberAmount = 10,
+                        BanAmount = 10,
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Fail
+                };
+            }
+        }
+
+        /// <summary>
+        /// 取得 查詢條件的會員列表
+        /// </summary>
+        [HttpPost]
+        public ResponseResult RetrieveMemberList(RetrieveMemberList_Dto request)
+        {
+            try
+            {
+                return new ResponseResult<RetrieveMemberList_ResultDto>()
+                {
+                    ErrorCode = ErrorCode.Success,
+                    Result = new RetrieveMemberList_ResultDto
+                    {
+                        MemberAmount = 1000,
+                        MemberList = new List<Member>()
+                        {
+                            new Member(){
+                                SerialNumber =1,
+                                CreateDateTime = DateTime.Now,
+                                MemberAccount = "account",
+                                MemberLevel =1,
+                                MuteReasonId = 0,
+                                NickName = "",
+                                OtherMuteReasonRemark = "",
+                            }
+                        }
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Fail
+                };
+            }
+        }
+
+        /// <summary>
+        /// 取得 特定會員的修改紀錄
+        /// </summary>
+        [HttpPost]
+        public ResponseResult RetrieveMemberModifyRecord(RetrieveMemberModifyRecord_Dto request)
+        {
+            try
+            {
+                return new ResponseResult<RetrieveMemberModifyRecord_ResultDto>
+                {
+                    ErrorCode = ErrorCode.Success,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Fail
+                };
+            }
+        }
+
+        /// <summary>
+        /// 修改會員等級
+        /// </summary>
+        [HttpPost]
+        public ResponseResult UpdateMemberLevel(UpdateMemberLevel_Dto request)
+        {
+            try
+            {
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Success
+                };
+            }
+            catch (Exception ex)
+            {
+                return new ResponseResult()
+                {
+                    ErrorCode = ErrorCode.Fail
+                };
+            }
+        }
+    }
+}
---
+++ CoreMvcVuePractice/Models/Dtos/MemberListControllerDtos.cs
@@ -0,0 +1,197 @@
+﻿namespace CoreMvcVuePractice.Models.Dtos
+{
+    public class MemberListControllerDtos
+    {
+        public class RetrieveMemberModeTypes_ResultDto
+        {
+            public IEnumerable<MemberMode>? MemberModeList { get; set; }
+        }
+
+        public class RetrieveBanReasonTypes_ResultDto
+        {
+            public IEnumerable<BanReason>? BanReasonList { get; set; }
+        }
+
+        public class RetrieveMemberSum_ResultDto
+        {
+            /// <summary>
+            /// 會員總數量
+            /// </summary>
+            public long MemberAmount { get; set; }
+
+            /// <summary>
+            /// 黑名單會員總數量
+            /// </summary>
+            public long BanAmount { get; set; }
+        }
+
+        public class RetrieveMemberList_Dto
+        {
+            /// <summary>
+            /// 查詢開始日期
+            /// </summary>
+            public DateTime? StartDateTime { get; set; }
+
+            /// <summary>
+            /// 查詢結束日期
+            /// </summary>
+            public DateTime? EndDateTime { get; set; }
+
+            /// <summary>
+            /// 會員ID
+            /// </summary>
+            public string? MemberAccount { get; set; }
+
+            /// <summary>
+            /// 禁言原因ID
+            /// </summary>
+            public int? MuteReasonId { get; set; }
+
+            /// <summary>
+            /// 會員等級
+            /// </summary>
+            public byte? MemberLevel { get; set; }
+
+            /// <summary>
+            /// 當前頁面索引
+            /// </summary>
+            public int NowPageIndex { get; set; }
+
+            /// <summary>
+            /// 當前頁面共幾筆
+            /// </summary>
+            public int PerpageRowAmount { get; set; }
+        }
+
+        public class RetrieveMemberList_ResultDto
+        {
+            /// <summary>
+            /// 會員總數量
+            /// </summary>
+            public long MemberAmount { get; set; }
+
+            /// <summary>
+            /// 會員列表
+            /// </summary>
+            public IEnumerable<Member>? MemberList { get; set; }
+        }
+
+        public class RetrieveMemberModifyRecord_Dto
+        {
+            /// <summary>
+            /// 會員流水號
+            /// </summary>
+            public long SerialNumber { get; set; }
+        }
+
+        public class RetrieveMemberModifyRecord_ResultDto
+        {
+
+        }
+
+        public class UpdateMemberLevel_Dto
+        {
+            public int SerialNumber { get; set; }
+
+            public int MemberLevel { get; set; }
+        }
+
+        public class MemberMode
+        {
+            /// <summary>
+            /// 會員模式代號
+            /// </summary>
+            public int SerialNumber { get; set; }
+            /// <summary>
+            /// 模式名稱
+            /// </summary>
+            public string? Name { get; set; }
+            /// <summary>
+            /// 排序
+            /// </summary>
+            public int Sort { get; set; }
+            /// <summary>
+            /// 備註
+            /// </summary>
+            public string? Remark { get; set; }
+            /// <summary>
+            /// 禮物ID集合
+            /// </summary>
+            public IEnumerable<int>? GiftSettingIds { get; set; }
+            /// <summary>
+            /// 建立日期
+            /// </summary>
+            public DateTime CreateDateTime { get; set; }
+            /// <summary>
+            /// 是否禁言
+            /// </summary>
+            public bool IsMute { get; set; }
+            /// <summary>
+            /// 類型
+            /// </summary>
+            public byte Type { get; set; }
+        }
+
+        public class BanReason
+        {
+            /// <summary>
+            /// 流水號
+            /// </summary>
+            public int SerialNumber { get; set; }
+            /// <summary>
+            /// 禁言原因
+            /// </summary>
+            public string? Reason { get; set; }
+            /// <summary>
+            /// 天數
+            /// </summary>
+            public int Day { get; set; }
+            /// <summary>
+            /// 排序
+            /// </summary>
+            public int Sort { get; set; }
+            /// <summary>
+            /// 建立日期
+            /// </summary>
+            public DateTime CreateDateTime { get; set; }
+        }
+
+        public class Member
+        {
+            /// <summary>
+            /// 查詢修改紀錄用
+            /// </summary>
+            public int SerialNumber { get; set; }
+
+            /// <summary>
+            /// 會員帳號
+            /// </summary>
+            public string? MemberAccount { get; set; }
+
+            /// <summary>
+            /// 暱稱
+            /// </summary>
+            public string? NickName { get; set; }
+
+            /// <summary>
+            /// 等級
+            /// </summary>
+            public int MemberLevel { get; set; }
+
+            /// <summary>
+            /// 產生時間
+            /// </summary>
+            public DateTime CreateDateTime { get; set; }
+
+            /// <summary>
+            /// 禁言原因ID
+            /// </summary>
+            public int? MuteReasonId { get; set; }
+
+            /// <summary>
+            /// 其他禁言原因備註
+            /// </summary>
+            public string? OtherMuteReasonRemark { get; set; }
+        }
+    }
+}
